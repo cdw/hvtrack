3fad4724412c7f5dbcf1bf54f7cb84bf6bd82254
Dave Williams <cdave@uw.edu>
2014-05-18 22:57:41 -0400

    Template for background subtraction class.

---
+++ hvtrack/background.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+""" Background.py
+Created by Dave Williams on 2013.11.07
+
+Background.py provides access to background subtraction mechanisms and their
+associated parameters. 
+"""
+
+import numpy as np
+import cv2
+
+class Background(object):
+    """Remember the background state for a video file"""
+    def __init__(self, vid):
+        """Get goin'."""
+        pass
